0487c809aca02abd00626939ab48d16514dae145
veronica <v.naranjo@utp.edu.co>
2024-12-16 23:55:35 -0500

    Updated Header component to include ThemeToggle for dark/light mode switching

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,31 +3,38 @@ import { Button } from "./ui/button";
 
 //components
 import Nav from "./Nav";
+import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto flex justify-between items-center">
+    <header className="py-8 xl:py-7 dark:text-white">
+        <div className="container mx-auto flex justify-between items-center dark:bg-[#1a2747b3]/40 bg-[#C8B6F8]/40 p-4 rounded-2xl">
 
             {/* logo */}
             <Link href="/">
-                <h1 className="text-4x1 font-semibold">
+                <h1 className="text-4x1 font-semibold ml-10">
                     Veronica<span className="text-accent">.</span>
                 </h1>
             </Link>
 
             {/* desktop navbar and hire me button */}
-            <div className="hidden xl:flex items-center gap-8">                
+            <div className="flex justify-center">                
                 <Nav />
-                <Link href="/contact">
+                {/* <Link href="/contact">
                 <Button>Contact</Button>
-                </Link>
+                </Link> */}
             </div>
 
             {/* mobile nav */}
             <div className="xl:hidden">
                 mobile nav
             </div>
+
+            {/* Dark/Light */}
+            <div className="flex justify-end">
+                <ThemeToggle />
+            </div>
+            
 
         </div>
     </header>
